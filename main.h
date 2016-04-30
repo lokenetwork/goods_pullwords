1eed7c9bb9fc0ec5b86cf1a7543897fa2fc3bfa8
loken <lokenetwork@users.noreply.github.com>
2016-04-30 19:11:39 +0800

    Create main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+//
+// Created by root on 4/30/16.
+//
+#ifndef PULLWORDS_MAIN_H
+#define PULLWORDS_MAIN_H
+#define MAXLINE 1024
+//分词结果的最大长度
+#define WORD_RESULT_LEN 200
+//可处理的最大的客户端发送的搜索词长度
+#define MAX_SEARCH_WORDS_LEN 200
+//最多开多少个线程处理分词
+#define MAX_CUT_WORD_THREAD_NUMBER 30
+//各线程分词结果结构体
+struct TREE_CUT_WORDS_RESULT {
+    //分词结果
+    char pullwords_result[MAX_SEARCH_WORDS_LEN];
+    //分词权重
+    int words_weight;
+};
+
+#endif //PULLWORDS_MAIN_H
